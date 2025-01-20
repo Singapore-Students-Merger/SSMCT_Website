9682700ca9868fa0b6ca3c2595c846fc2b22a5b6
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2025-01-20 22:54:04 +0800

    im gonna commit masssss genocide

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), 'zlib-sync'];
+    return config;
+  },
   images: {
     remotePatterns: [
       {
