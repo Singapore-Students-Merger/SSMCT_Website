c206274bf08a2d221ee7eeee8ded8311592ee637
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2025-01-27 21:23:48 +0800

    forgot about the acheivements

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -36,5 +36,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.5,
     },
+    {
+      url: `${WEBSITE_URL}/acheivements`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
   ]
 }
