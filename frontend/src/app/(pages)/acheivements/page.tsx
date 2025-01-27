c36a1f1166b904312ef0e3c1c1e8d5b0b4fbcf9e
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2025-01-27 21:19:18 +0800

    formatted and added backend

--- frontend/src/app/(pages)/acheivements/page.tsx
+++ frontend/src/app/(pages)/acheivements/page.tsx
@@ -1,49 +1,50 @@
 import Hero from "../../../components/Hero";
 import AchievementCard from "../../../components/AchievementCard";
+import { getAllCtfs } from "@/app/api/acheivements/route";
 
-export default function AchievementsPage() {
-  const achievements = [
-    {
-      title: "1st Place",
-      points: 100000,
-      participants: ["Chan Si Yu David", "Chan Si Yu David", "Chan Si Yu David"],
-      icon: "/backgrounds/home.png",
-    },
-    {
-      title: "1st Place",
-      points: 100000,
-      participants: ["Chan Si Yu David", "Chan Si Yu David", "Chan Si Yu David"],
-      icon: "/backgrounds/home.png",
-    },
-    {
-      title: "1st Place",
-      points: 100000,
-      participants: ["Chan Si Yu David", "Chan Si Yu David", "Chan Si Yu David"],
-      icon: "/backgrounds/home.png",
-    },
-  ];
+export default async function AchievementsPage() {
+  const achievements = await getAllCtfs();
 
   return (
     <div>
       <Hero
-        bgImage="/backgrounds/home.png"
-        title="Singapore Students Merger"
-        subtitle="Our Achievements"
+        bgImage="/backgrounds/acheivements.jpg"
+        bgImageMobile="/backgrounds/acheivements_mobile.jpg"
+        title="Our Achievements"
       />
       <div className="bg-gray-900 py-12">
         <div className="max-w-6xl mx-auto px-4">
-          <h2 className="text-3xl font-bold text-white mb-6">2024</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {achievements.map((achievement, index) => (
-              <AchievementCard
-                key={index}
-                title={achievement.title}
-                points={achievement.points}
-                participants={achievement.participants}
-                icon={achievement.icon}
-              />
-            ))}
-          </div>
+          <section className="mb-12">
+            <h2 className="text-5xl font-bold text-white mb-6">2025</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {(achievements['2025']??[]).map((achievement, index) => (
+                <AchievementCard
+                  key={index}
+                  title={achievement.title}
+                  points={achievement.points}
+                  participants={achievement.participants}
+                  icon={achievement.logo ?? "/assets/no_logo.webp"}
+                  placing={achievement.placing}
+                />
+              ))}
+            </div>
+          </section>
+          <section>
+            <h2 className="text-5xl font-bold text-white mb-6">2024</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {achievements['2024'].map((achievement, index) => (
+                <AchievementCard
+                  key={index}
+                  title={achievement.title}
+                  points={achievement.points}
+                  participants={achievement.participants}
+                  icon={achievement.logo ?? "/assets/no_logo.webp"}
+                  placing={achievement.placing}
+                  link={achievement.link}
+                />
+              ))}
+            </div>
+          </section>
         </div>
       </div>
     </div>
--- frontend/src/app/api/acheivements/route.ts
+++ frontend/src/app/api/acheivements/route.ts
@@ -1,10 +1,147 @@
 import { AchievementDetails } from "@/types/acheivements";
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from "@/lib/prisma";
+
+export async function getAllCtfs() {
+  const data = await prisma.ctf.findMany(
+    {
+      orderBy: [{
+        placing: "asc",
+      },
+      {
+        event: {
+          date: "desc"
+        }
+      }],
+      where: {
+        NOT: {
+          points: null
+        }
+      },
+      select: {
+        event: {
+          select: {
+            title: true,
+            description: true,
+            date: true,
+            roles: {
+              where: {
+                type: "ctf participant"
+              },
+              select: {
+                linkedDiscordRole: {
+
+                  select: {
+                    userDiscordRoles: {
+                      select: {
+                        discordUser: {
+                          select: {
+                            id: true,
+                            username: true,
+                            user: {
+                              select: {
+                                realName: true
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                },
+                userRoles: {
+                  select: {
+                    user: {
+                      select: {
+                        id: true,
+                        name: true,
+                        realName: true,
+                        discordId: true,
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
+        },
+        members: {
+          select: {
+            user: {
+              select: {
+                name: true,
+                discordId: true,
+                realName: true,
+              },
+            }
+          }
+        },
+        ctfId: true,
+        points: true,
+        placing: true,
+        link: true,
+        logo: true,
+        participants: true,
+      }
+    }
+  );
+  const formattedData = data.map((row) => {
+    const participantingMembers: {
+      [discordId: string]: {
+        name?: string,
+        realName?: string,
+      }
+    } = {}
+    for (const member of row.members) {
+      if (!member.user.discordId) continue;
+      participantingMembers[member.user.discordId] = {
+        name: member.user.name ?? undefined,
+        realName: member.user.realName ?? undefined,
+      };
+    }
+    for (const member of row.event.roles[0]?.linkedDiscordRole?.userDiscordRoles ?? []) {
+      if (!member.discordUser.id) continue;
+      participantingMembers[member.discordUser.id] = {
+        name: member.discordUser.username ?? undefined,
+        realName: member.discordUser.user?.realName ?? undefined,
+      };
+    }
+    for (const member of row.event.roles[0]?.userRoles ?? []) {
+      if (!member.user.discordId) continue;
+      participantingMembers[member.user.discordId] = {
+        name: member.user.name ?? undefined,
+        realName: member.user.realName ?? undefined,
+      };
+    }
+    return {
+      link: row.link,
+      logo: row.logo,
+      points: row.points,
+      placing: row.placing,
+      ctfId: row.ctfId,
+      title: row.event.title,
+      description: row.event.description,
+      participants: Object.values(participantingMembers),
+      date: row.event.date,
+    }
+  })
+
+  const seperatedByYear: {
+    [year: string]: AchievementDetails[]
+  } = {}
+  for (const row of formattedData) {
+    const year = row.date.getFullYear().toString();
+    if (!seperatedByYear[year]) {
+      seperatedByYear[year] = [];
+    }
+    seperatedByYear[year].push(row);
+  }
+  return seperatedByYear;
+}
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const fields = searchParams.get("fields");
-  const showAll = searchParams.get("all");
+  // const showAll = searchParams.get("all");
   try {
     if (fields === "names") {
       // Fetch only eventId and eventName
@@ -25,34 +162,7 @@ export async function GET(req: NextRequest) {
 
       return NextResponse.json(formattedCtfs);
     }
-    const achievementDetails = await prisma.ctf.findMany(
-      {
-        select: {
-          event: {
-            select: {
-              title: true,
-              description: true,
-              date: true,
-            },
-          },
-          members: {
-            select: {
-              user: {
-                select: {
-                  name: true,
-                },
-              }
-            }
-          },
-          ctfId: true,
-          points: true,
-          placing: true,
-          link: true,
-          logo: true,
-          participants: true,
-        }
-      }
-    );
+    const achievementDetails = await getAllCtfs()
     return NextResponse.json(achievementDetails);
   }
   catch (error) {
