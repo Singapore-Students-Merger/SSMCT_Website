d9928545270d062edccfcda453fe0e9202a4373d
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2025-02-04 23:35:23 +0800

    oops forgot some small stuff

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,16 +4,16 @@ export default function Footer() {
     return (
         <footer className="bg-background text-white bg-secondary-tier3/80 text-xl font-bold py-4">
             <div className="flex gap-8 justify-center">
-                <Link href="https://www.instagram.com/singaporestudentsmerger/" className='cursor-pointer hover:brightness-75 transition-all duration-500 flex gap-2 justify-center items-center'>
-                    <SocialIcon network="instagram" url="https://www.instagram.com/singaporestudentsmerger/" />
+                <Link target ="_blank" href="https://www.instagram.com/singaporestudentsmerger/" className='cursor-pointer hover:brightness-75 transition-all duration-500 flex gap-2 justify-center items-center'>
+                    <SocialIcon as="div" network="instagram" url="https://www.instagram.com/singaporestudentsmerger/" />
                         @singaporestudentsmerger
                 </Link>
-                <Link href="https://discord.gg/SWAwuFVQ6M" className='cursor-pointer hover:brightness-75 transition-all duration-500 flex gap-2 justify-center items-center'>
-                    <SocialIcon network="discord" url="https://discord.gg/SWAwuFVQ6M" />
+                <Link target ="_blank" href="https://discord.gg/SWAwuFVQ6M" className='cursor-pointer hover:brightness-75 transition-all duration-500 flex gap-2 justify-center items-center'>
+                    <SocialIcon as="div" network="discord" url="https://discord.gg/SWAwuFVQ6M" />
                         Recruitment Discord
                 </Link>
-                <Link href="https://www.linkedin.com/company/ssmctsg/" className='cursor-pointer hover:brightness-75 transition-all duration-500 flex gap-2 justify-center items-center'>
-                    <SocialIcon network="linkedin" url="https://www.linkedin.com/company/ssmctsg/" />
+                <Link target ="_blank" href="https://www.linkedin.com/company/ssmctsg/" className='cursor-pointer hover:brightness-75 transition-all duration-500 flex gap-2 justify-center items-center'>
+                    <SocialIcon as="div" network="linkedin" url="https://www.linkedin.com/company/ssmctsg/" />
                     Singapore Students Merger
                 </Link>
             </div>
