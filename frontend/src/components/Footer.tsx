6e9e4793c6c013e4333aeca8fc8ddc8ff964538a
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2025-02-04 23:38:14 +0800

    forgot about mobile again

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,8 +2,8 @@ import { SocialIcon } from 'react-social-icons';
 import Link from 'next/link';
 export default function Footer() {
     return (
-        <footer className="bg-background text-white bg-secondary-tier3/80 text-xl font-bold py-4">
-            <div className="flex gap-8 justify-center">
+        <footer className="bg-background text-white bg-secondary-tier3/80 text-xl font-bold py-4 w-full">
+            <div className="flex gap-8 justify-start md:justify-center w-full flex-wrap px-4">
                 <Link target ="_blank" href="https://www.instagram.com/singaporestudentsmerger/" className='cursor-pointer hover:brightness-75 transition-all duration-500 flex gap-2 justify-center items-center'>
                     <SocialIcon as="div" network="instagram" url="https://www.instagram.com/singaporestudentsmerger/" />
                         @singaporestudentsmerger
