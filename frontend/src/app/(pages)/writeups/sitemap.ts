966d48286e9991555bee4da4838b2f2b00533e22
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2025-02-05 23:43:08 +0800

    proddddd

--- frontend/src/app/(pages)/writeups/sitemap.ts
+++ frontend/src/app/(pages)/writeups/sitemap.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma"
 
 const WEBSITE_URL = process.env.NEXT_PUBLIC_WEBSITE_URL
-export const revalidate = 3600;
+export const dynamic = 'force-dynamic'
 
 export default async function sitemap(){
     const writeups = await prisma.writeups.findMany({
